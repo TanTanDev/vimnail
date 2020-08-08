95d6daba303ba877e1b7102c1047d4c788f9fd49
rainqubit <rainqubit@gmail.com>
2020-08-09 01:44:55 +0700

    save_image path to os specific and print error

--- src/board.rs
+++ src/board.rs
@@ -4,6 +4,7 @@ use crate::item_collection::ItemCollection;
 use ggez::{Context, GameResult, graphics};
 use ggez::conf;
 use image::{DynamicImage, ImageBuffer};
+use std::path::Path;
 
 pub struct Board {
     pub item_collection: ItemCollection,
@@ -32,8 +33,8 @@ impl Board {
         }
     }
 
-    pub fn save_image(&mut self, ctx: &mut Context, path: &String){
-        //TODO: Add actual error handling, no unwrap() shenanigans!
+    pub fn save_image(&mut self, ctx: &mut Context, path: &String) {
+        //TODO: Add better error handling and bubling the error into UI
 
         let (graphic_width, graphic_height) = graphics::drawable_size(ctx);
         
@@ -44,27 +45,31 @@ impl Board {
         self.draw(ctx).unwrap();
         ggez::graphics::present(ctx).unwrap();
 
-        // capture the image from canvas
+        // capture the image from canvas into raw vector
         let canvas = buffer_canvas.unwrap();
         let canvas_image = canvas.into_inner();
 
-        // convert the image to rgba 8 bit vector
+        // convert the image to DynamicImage from raw vector
         let image_rgba = canvas_image.to_rgba8(ctx).unwrap();
-
-        // turn the raw rgba8 into DynamicImage
         let image_buffer = DynamicImage::ImageRgba8(ImageBuffer::from_raw(graphic_width as u32, graphic_height as u32, image_rgba).unwrap());
 
-        // flip the image cause the raw image vector is flipped.. dunno why *shrug*
+        // flip the image because the raw image vector is flipped.. dunno why ¯\_(ツ)_/¯
         let image_buffer = image_buffer.flipv();
 
         //save the image
-        //TODO: change it to parameter and convert path to OS specific
-        image_buffer.save(path).unwrap();
+        let image_path = Path::new(path).as_os_str();
+        
+        match image_buffer.save(image_path) {
+            Ok(_) => {
+                println!("Image saved successfully!")
+            }
+            Err(err) => {
+                println!("Failed to save image : {:?}", err);
+            }
+        }
 
         //return to drawing into screen
         graphics::set_canvas(ctx, Option::None);
-
-
     }
 
     pub fn draw(&self, ctx: &mut Context) -> GameResult {
